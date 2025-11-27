5be8fa02bd95496f88b3da400c626a76b8983428
Frusadev <frusadev@gmail.com>
2025-11-27 23:00:43 +0000

    add utility errors

---
+++ lib/errors/access-errors.ts
@@ -0,0 +1,23 @@
+export class BillingPlanIncompatible extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+export class InactiveSubscriptionError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
+
+export class AccessDeniedError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = this.constructor.name;
+    Object.setPrototypeOf(this, new.target.prototype);
+  }
+}
