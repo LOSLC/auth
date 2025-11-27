2d273da0250f404f29305fc3222c2f0cec0a487c
Frusadev <frusadev@gmail.com>
2025-11-27 22:30:56 +0000

    chore(db): add global drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from "drizzle-kit";
+import { getEnv } from "./core/utils/env";
 
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./core/db/schema.ts",
+  schema: "./core/db/schemas.ts",
   out: "./core/db/migrations",
+  dbCredentials: {
+    url: getEnv("DATABASE_URL")
+  }
 });
